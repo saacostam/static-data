9030b2040488f064d666127b54af158e79e9fccb
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-07-21 04:40:25 -0500

    feat(projects): create project list data

--- src/modules/projects/types/project.type.ts
+++ src/modules/projects/types/project.type.ts
@@ -1,3 +1,44 @@
+/**
+ * Project
+ *
+ * @export
+ * @interface Project
+ * @typedef {Project}
+ */
 export interface Project {
+    /**
+     * Identifier
+     *
+     * @type {string}
+     */
+    id: string;
 
+
+    /**
+     * Name of the project
+     *
+     * @type {string}
+     */
+    name: string;
+
+    /**
+     * A rating (from 0 to 10) given by me that represents how complex and useful a project is.
+     * Ultimately, it prioritizes projects the project that I want to share the most.
+     * 
+     */
+    rating: number;
+
+    /**
+     * A small-size image (300x150) for a project.
+     *
+     * @type {?string}
+     */
+    smImage?: string;
+
+    /**
+     * A medium-size image (1200x600) for a project.
+     *
+     * @type {?string}
+     */
+    mdImage?: string;
 }
